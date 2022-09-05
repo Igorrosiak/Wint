31fa9ae3f7fe0e777590191c0adb09b90d6a5262
Onipotente <igorluizbelterrosiak@gmail.com>
2022-09-05 12:57:30 -0300

    Creating Back-End with Initialzr

---
+++ Back-End/src/main/java/br/wint/WintApplication.java
@@ -0,0 +1,13 @@
+package br.wint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WintApplication.class, args);
+	}
+
+}
