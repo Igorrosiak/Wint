74e4a8a505df0a02560b8442dbe39eb0154930e3
Igor Rosiak <86728719+Igorrosiak@users.noreply.github.com>
2022-09-07 12:58:44 -0300

    Utilizing Lombok

--- Back-End/src/main/java/br/wint/model/User.java
+++ Back-End/src/main/java/br/wint/model/User.java
@@ -1,9 +1,16 @@
 package br.wint.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+@Getter
+@Setter
+@ToString
 @Entity(name = "user")
 public class User {
 
