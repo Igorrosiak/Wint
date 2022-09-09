a1eb0b315861661772e63cd7343b3f5030504eb6
MatheusMMarques <matheus_m_reis@estudante.sc.senai.br>
2022-09-09 16:17:51 -0300

    19 - Tela Finalizada

--- Front-End Web/src/views/signIn/signInView.jsx
+++ Front-End Web/src/views/signIn/signInView.jsx
@@ -1,6 +1,8 @@
 import "./signInView.scss"
 import imagem from "../../assets/images/logo-wint.png"
 import textWint from "../../assets/images/texto-wint.png"
+import imgAppStore from "../../assets/images/imgAppStore.png"
+import imgGooglePlay from "../../assets/images/imgGooglePlay.png"
 
 export const SignInView = _ => {
     return (
@@ -17,9 +19,15 @@ export const SignInView = _ => {
                     <input type="text" placeholder="  SENHA" />
                     <small>ESQUECEU A SENHA?</small>
                     <button>ENTRAR</button>
-                    <p>NÃO TEM UMA CONTA? <a href="canva.com">CADASTRE-SE</a></p>
-                    <img src="#" alt="Imagem App Store" />
-                    <img src="#" alt="Imagem Google Play" />
+                    <div className="texto">
+                        <p>NÃO TEM UMA CONTA? <a href="canva.com">CADASTRE-SE</a></p>
+                    </div>
+                <article className="images">
+                    <div>
+                        <img className="appStore" src={imgAppStore} alt="Imagem App Store" />
+                        <img className="googlePlay" src={imgGooglePlay} alt="Imagem Google Play" />
+                    </div>
+                </article>
                 </div>
             </article>
         </main>
