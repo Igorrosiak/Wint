eb517a1b87f5223503d4bc11a75a4761af941a06
MATHEUS MACHADO MARQUES DOS REIS <matheus_m_reis@estudante.sc.senai.br>
2022-09-14 13:00:03 -0300

    atualizando

--- Front-End Web/src/views/profile/profileView.jsx
+++ Front-End Web/src/views/profile/profileView.jsx
@@ -4,32 +4,55 @@ export const ProfileView = _ => {
     return (
         <main className="profile">
             <h2>EDITAR PERFIL</h2>
+            <div className="main">
+                <form className="formulario">
+                    <div className="container">
+                        <div className="text-area">
+                            <label class="required" for="name">Nome</label>
+                            <input id="name" class="input" name="name" type="text" value="" size="30" placeholder="ariany" />
+                        </div>
 
-            <div>
-                <label class="required" for="name">Nome:</label>
-                <input id="name" class="input" name="name" type="text" value="" size="30" placeholder="ariany"/>
-                
-                <label class="required" for="sobrenome">Sobrenome:</label>
-                <input id="lastname" class="input" name="lastname" type="text" value="" size="30" />
-        
-                <label class="required" for="name">Nome usuário:</label>
-                <input id="username" class="input" name="username" type="text" value="" size="30" />
-                
-                <label class="required" for="sobrenome">Biografia:</label>
-                <input id="email" class="input" name="email" type="text" value="" size="30" />
+                        <div className="text-area">
+                            <label class="required" for="sobrenome">Sobrenome</label>
+                            <input id="lastname" class="input" name="lastname" type="text" value="" size="30" />
+                        </div>
+                    </div>
+    
+                    <div className="container">
+                        <div className="text-area">
+                            <label class="required" for="name">Nome Usuário</label>
+                            <input id="username" class="input" name="username" type="text" value="" size="30" />
+                        </div>
+                        <div className="text-area">
+                            <label class="required" for="sobrenome">Biografia</label>
+                            <input id="email" class="input" name="email" type="text" value="" size="30" />
+                        </div>
+                    </div>
 
-                <label class="required" for="sobrenome">Email:</label>
-                <input id="email" class="input" name="email" type="text" value="" size="30" />
+                    <div className="container">
+                        <div className="text-area">
+                            <label class="required" for="sobrenome">Email</label>
+                            <input id="email" class="input" name="email" type="text" value="" size="30" />
+                        </div>
+    
+                        <div className="text-area">
+                            <label class="required" for="telefone">telefone</label>
+                            <input id="email" class="input" name="telefone" type="number" value="" size="30" />
+                        </div>
+                    </div>
 
-                <label class="required" for="sobrenome">Telefone:</label>
-                <input id="email" class="input" name="email" type="text" value="" size="30" />
-
-                <label class="required" for="sobrenome">Alterar senha:</label>
-                <input id="email" class="input" name="email" type="text" value="" size="30" />
-
-                <label class="required" for="sobrenome">Repetir senha:</label>
-                <input id="email" class="input" name="email" type="text" value="" size="30" />
-
+                    <div className="container">
+                        <div className="text-area">
+                            <label class="required" for="sobrenome">Alterar Senha</label>
+                            <input id="email" class="input" name="email" type="text" value="" size="30" />
+                        </div>
+    
+                        <div className="text-area">
+                            <label class="required" for="sobrenome">Repetir Senha</label>
+                            <input id="email" class="input" name="email" type="text" value="" size="30" />
+                        </div>
+                    </div>
+                </form>
             </div>
         </main>
     )
