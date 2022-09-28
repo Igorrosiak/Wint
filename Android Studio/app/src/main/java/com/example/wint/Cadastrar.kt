ceb8e4b8f55e439ec6c7e8d3c1ae183260edbd63
arianymello <arianychagas05@gmail.com>
2022-09-28 11:40:09 -0300

    29 - inicio tela cadastrar

--- Android Studio/app/src/main/java/com/example/wint/Cadastrar.kt
+++ Android Studio/app/src/main/java/com/example/wint/Cadastrar.kt
@@ -2,15 +2,10 @@ package com.example.wint
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.service.autofill.OnClickAction
-import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
-
+class Cadastrar : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_cadastrar_main)
     }
-
 }
