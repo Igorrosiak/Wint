fb45ebd175f254e8fa688a5bc6e6ec6f382191e9
ARIANY DE MELLO CHAGAS <arianychagas05@gmail.com>
2022-10-05 12:04:38 -0300

    29 - criação do feedActivity

--- Android Studio/app/src/main/java/com/example/wint/FeedActivity.java
+++ Android Studio/app/src/main/java/com/example/wint/FeedActivity.java
@@ -2,7 +2,9 @@ package com.example.wint;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class FeedActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class FeedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+    }
+    public void sair(View v){
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
     }
 }
