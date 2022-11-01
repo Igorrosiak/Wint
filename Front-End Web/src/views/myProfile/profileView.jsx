c8bf0f7f3234f75e2ab7398bb89aedd6b7c4db1d
MATHEUS MACHADO MARQUES DOS REIS <matheus_m_reis@estudante.sc.senai.br>
2022-11-01 09:50:23 -0300

    21/inicio Works

--- Front-End Web/src/views/myProfile/profileView.jsx
+++ Front-End Web/src/views/myProfile/profileView.jsx
@@ -1,11 +1,13 @@
 import {HeaderComponent} from '../../widgets/HeaderComponent'
 import {SkillsComponent} from '../../widgets/SkillsComponent'
+import {WorksComponent} from '../../widgets/WorksComponent'
 
 export const ProfileView = _ => {
     return (
         <>
             {/* <HeaderComponent /> */}
-            <SkillsComponent />
+            {/* <SkillsComponent /> */}
+            <WorksComponent/>
         </>
     )
 }
---
+++ Front-End Web/src/widgets/WorksComponent.jsx
@@ -0,0 +1,21 @@
+import '../assets/scss/works.scss'
+
+export const WorksComponent = _ => {
+    return(
+        <article className="main">
+
+            <h4>-WORKS</h4>
+
+            <div className="box-header">
+                <div className="box">
+
+                </div>
+
+                <div className="box">
+
+                </div>
+
+            </div>
+        </article>
+    )
+}
