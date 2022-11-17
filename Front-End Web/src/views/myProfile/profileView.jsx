a90e2b9ba6978b843781d0b7302e94ef51a75601
GABRIELA MOREIRA SENNA <gabriela_senna@estudante.sc.senai.br>
2022-11-17 09:53:43 -0300

    tela loading

--- Front-End Web/src/views/myProfile/profileView.jsx
+++ Front-End Web/src/views/myProfile/profileView.jsx
@@ -2,14 +2,27 @@ import { HeaderComponent } from '../../widgets/HeaderComponent'
 import { ProfExpComponent } from '../../widgets/ProfExpComponent'
 import { SkillsComponent } from '../../widgets/SkillsComponent'
 import { WorksComponent } from '../../widgets/WorksComponent'
+import { useState, useEffect } from 'react'
+import Animation from '../../widgets/TelaLoading'
 
 export const ProfileView = _ => {
+    const [animaionDidEnd, setAnimationDidEnd] = useState (false) 
+    useEffect(() => {
+        setTimeout(() => {
+          setAnimationDidEnd(true);
+        }, 9000);
+      });
+
     return (
         <>
-            {/* <HeaderComponent /> */} 
-            <SkillsComponent /> 
-            <ProfExpComponent />
-            {/* <WorksComponent/> */}
+            { animaionDidEnd ?
+                <>
+                <HeaderComponent /> 
+                <SkillsComponent /> 
+                <ProfExpComponent />
+                <WorksComponent/>
+                </> : <Animation /> 
+            }
         </>
     )
 }
