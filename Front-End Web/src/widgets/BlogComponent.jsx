dff323da3028f8ea7a2f57aa0615aec9c4705746
MatheusMMarques <matheus_m_reis@estudante.sc.senai.br>
2022-11-21 20:21:35 -0300

    49/Dsign Finalizado

--- Front-End Web/src/widgets/BlogComponent.jsx
+++ Front-End Web/src/widgets/BlogComponent.jsx
@@ -11,15 +11,39 @@ export const BlogComponent = _ => {
             <div className="images-box">
 
                 <div className="box">
-                    <img src={ImgLeft} alt="left"/>
+                    <img src={ImgLeft} alt="left" />
+                    <p className="desc-1">Como desenvolver um protótipo interativo</p>
+                    <br />
+                    <p className="date-1">Dez 15, 2021</p>
+                    <br />
+                    <p className='txt-1'>Lorem ipsum  dolor sit amet, consectetur adipiscing elit. Fusce id magna nec ante scelerisque laoreet. Fusce finibus ...
+                    </p>
+                    <br />
+                    <p className='p-1'>Continuar lendo</p>
                 </div>
 
                 <div className="box">
-                    <img src={ImgCenter} alt="center"/>
+                    <img src={ImgCenter} alt="center" />
+                    <p className="desc-2">Seja mais produtivo com Notion</p>
+                    <br />
+                    <p className="date-2">Ago 8, 2020</p>
+                    <br />
+                    <p className='txt-2'>Lorem ipsum  dolor sit amet, consectetur adipiscing elit. Fusce id magna nec ante scelerisque laoreet. Fusce finibus ...
+                    </p>
+                    <br />
+                    <p className='p-2'>Continuar lendo</p>
                 </div>
 
                 <div className="box">
-                    <img src={ImgRight} alt="right"/>
+                    <img src={ImgRight} alt="right" />
+                    <p className="desc-3">Criando um Accordion em ReactJS</p>
+                    <br />
+                    <p className="date-3">Out 10, 2022</p>
+                    <br />
+                    <p className='txt-3'>Lorem ipsum  dolor sit amet, consectetur adipiscing elit. Fusce id magna nec ante scelerisque laoreet. Fusce finibus ...
+                    </p>
+                    <br />
+                    <p className='p-3'>Continuar lendo</p>
                 </div>
 
             </div>
